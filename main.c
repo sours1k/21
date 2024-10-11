f6b875ead5aee17113e7c6a241b86bb7a50e6c9c
sours1k <88582774+sours1k@users.noreply.github.com>
2024-10-11 11:45:38 +0500

    Update main.c

--- main.c
+++ main.c
@@ -4,25 +4,32 @@
 int main(){
     int n = 3, m = 3;
     int Arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    // int Temp[n][m];
+    int Temp[n][m];
+    for (int i = 0; i < n; i++){
+        if (i % 2 == 0) {
+            for (int k = 0; k < m; k++){
+                // printf("%d ", Arr[i][k]);
+                Temp[i][k] = Arr[i][k];
+            }
+        } else {
+            int cnt = 0;
+            for (int k = n; k > 0; k--){
+                // printf("%d ", Arr[i][k - 1]);
+                Temp[i][cnt] = Arr[i][k - 1];
+                cnt++;
+            }
+        }
+        printf("\n");
+    }
+
     for (int i = 0; i < n; i++){
         for (int k = 0; k < m; k++){
-            printf("[k:%d][i:%d]:%d", k, i, Arr[k][i]);
+            printf("%d", Temp[i][k]);
             if (k != 3){
                 printf(" ");
             }
         }
-            printf("\n");
+        printf("\n");
     }
-
-    // for (int i = 0; i < n; i++){
-    //     for (int k = 0; k < m; k++){
-    //         printf("%d", Temp[i][k]);
-    //         if (k != 3){
-    //             printf(" ");
-    //         }
-    //     }
-    //     printf("\n");
-    // }
     return 0;
 }
